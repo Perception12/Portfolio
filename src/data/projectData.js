45ae7e4866661e04e4b15f27ecbdcb80e8f7e3b9
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2022-12-13 10:31:22 +0100

    Update projectData.js

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -2,49 +2,49 @@ const projectData = [
     {
         "title": "Scholars Breed",
         "tools": ["React", "Firebase"],
-        "source": "https://github.com/Perception12/digital-lock-circuit",
-        "live": ""
+        "github": "#",
+        "live": "https://www.scholarsbreed.info"
     },
     {
         "title": "Random Quote Generator",
         "tools": ["React", "CSS"],
-        "source": "https://github.com/Perception12/Fitness-App",
+        "github": "https://github.com/Perception12/Fitness-App",
         "live": "https://random-quote-generator-kappa.vercel.app/"
     },
     {
         "title": "Fitness App",
         "tools": ["Flutter", "Dart"],
-        "source": "https://github.com/Perception12/random-quote-generator",
+        "github": "https://github.com/Perception12/random-quote-generator",
         "live": ""
     },
     {
         "title": "MarkDown Previewer",
         "tools": ["React", "Prism", "Marked"],
-        "source": "https://codepen.io/perception12/pen/bGMbemy",
+        "github": "https://codepen.io/perception12/pen/bGMbemy",
         "live": "https://codepen.io/perception12/full/bGMbemy"
     },
     {
         "title": "Calculator",
         "tools": ["React", "SCSS"],
-        "source": "https://codepen.io/perception12/pen/QWrLEMR",
+        "github": "https://codepen.io/perception12/pen/QWrLEMR",
         "live": "https://codepen.io/perception12/full/QWrLEMR"
     },
     {
         "title": "E-Millenial Store",
         "tools": ["HTML", "CSS", "JS"],
-        "source": "https://github.com/Perception12/E-millenial-store",
+        "github": "https://github.com/Perception12/E-millenial-store",
         "live": "https://perception12.github.io/E-millenial-store/"
     },
     {
         "title": "Banking App",
         "tools": ["Flutter", "Dart"],
-        "source": "https://github.com/Perception12/flutter_banking_ui",
+        "github": "https://github.com/Perception12/flutter_banking_ui",
         "live": ""
     },
     {
         "title": "LCD Arcade Game",
         "tools": ["Arduino", "RTOS", "C/C++"],
-        "source": "https://github.com/Perception12/Arduino-Projects/tree/main/lcdArcadeGame",
+        "github": "https://github.com/Perception12/Arduino-Projects/tree/main/lcdArcadeGame",
         "live": ""
     },
 
